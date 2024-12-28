f96abeab41d7435ddbfed95b0c966e6d8705e03b
Alexander Ramirez <alexramirez8962@gmail.com>
2024-12-28 02:40:07 -0500

    refactoring

---
+++ QuizBowl/proctor.py
@@ -0,0 +1,55 @@
+from typing import Any
+from logging import log
+import guesser
+import buzzer
+import json
+
+
+class Proctor: 
+    def __init__ (self, model: guesser, buzz: buzzer.Buzzer):
+        self.question = None   
+        self.answer = None
+        self.tot_len = None
+        self.guesser = model
+        self.buzzer = buzz
+        self.cur_len = 0
+        damn = []
+
+
+    def score(self, confidence: float, guess: str) -> float:
+        adj_conf = (1 - (self.cur_len + 1 / self.tot_len + 1)) * confidence
+        
+        if guess == self.answer:
+            return adj_conf
+        else:
+            return -adj_conf
+    
+    def guess(self) -> tuple[str, float]:
+        if self.cur_len > self.tot_len: 
+            self.cur_len = self.tot_len
+        cur = " ".join(self.question[:(self.cur_len + 1)])        
+        ans = self.guesser(cur, 1, 'simple')
+        guess = ans[0]["answer"]
+        conf = ans[0]["confidence"]
+        return guess, conf
+
+
+    def new_question(self, question: dict) -> None:
+        self.question = question["text"].split(" ")
+        self.answer = question["answer"]
+        self.tot_len = len(self.question)
+        self.cur_len = 10
+    
+
+    def __call__(self, question: str, freq: int, thresh: int) -> tuple[int, str]:
+        self.new_question(question)
+        while self.cur_len < self.tot_len: 
+            guess, conf = self.guess()            
+            if conf >= thresh: 
+                return self.score(conf, guess), guess
+            self.cur_len += freq
+        guess, conf = self.guess()            
+        return self.score(conf, guess), guess
+
+
+
---
+++ QuizBowl/qbam.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+import torch as th
+import nltk
+import numpy as np
+import pandas as pd
+from proctor import Proctor
+from buzzer import Buzzer
+from guesser import BertGuess
+import json
+
+class QBAM:
+
+    def __init__(self):
+        self.model = BertGuess(True)
+        self.buzzer = Buzzer()    
+        self.proctor = Proctor(self.model, self.buzzer)
+
+
+    def __call__(self, question: dict) -> Tuple[int, str]:
+        self.proctor.new_question(question)
+        return self.proctor(question, 1, 300)
+
+
+    def batch_guess(self, question_text: List[str]) -> List[Tuple[int, str]]:
+        guesses = []
+        for question in question_text:
+            guess = self(question)
+            guesses.append(guess)
+        return guesses
+ 
+     # allow for a json file to be passed in and so the model can be trained on that data and make predictions
+    def json_to_question(self, json_file: str) -> list[dict["text":str, "answer": str]]:
+        with open(json_file) as read:
+            data = json.load(read)
+            for question in data:
+                answer = question['answer']
+                text = question['text']
+        pass
+    # pkl files are used to save the model
+    def save(self):
+        pass
+    # pkl files are used to load the model
+    def load(self):
+        pass
+    def evaluate(self, json_file: str) -> dict:
+        pass
+
+
+    # Fine-tuning the model
+    ## 5. choose buzzer mode, static or dynamic
+    ## 6. fit the buzzer
+    ## 7. save the buzzer
+    ## 8. adjust preprocessing aggression, for example not remolding stop words, or adjusting stop word bag i.e the words that will be removed from the document
+    ## 9. adjust how often the proctor will ask the model the questions 
+    ## 10. guesser fine-tuning please help me
+
+if __name__ == "__main__":
+        question = "A speaker of one of this man's poems eats \"reality sandwiches\" and admits \"a naked lunch is natural to us.\" The speaker of another of this man's poems asserts that \"Death is that remedy all singers dream of\" as he walks in Greenwich Village thinking of his mother Naomi. The refrain \"I'm with you in Rockland\" appears in another poem by this author of \"Kaddish.\" That poem by this man begins, \"I saw the best (*) minds of my generation destroyed by madness.\" For 10 points, name this Beat poet who wrote \"Howl.\"" 
+        answer= "Allen Ginsberg"
+        question = {"text": question, "answer": answer}
+        model = QBAM()
+
+        score, guess = model(question)
+        print(f"Prediction: {guess}, Score: {score}") 
+        print(f"Answer: {answer}") 
+
+    
