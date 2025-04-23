ceb0781f671f22cd231178587b47f772eb11063d
Alexander Ramirez <alexramirez8962@gmail.com>
2025-04-22 21:05:23 -0400

    Added duplicate detection function

--- QuizBowl/contextGenerator.py
+++ QuizBowl/contextGenerator.py
@@ -3,6 +3,7 @@ from pyserini.search.lucene import LuceneSearcher
 import json
 import spacy
 import utils 
+import re
 # enwiki-paragraphs
 # wikipeia-dpr
 # wikipedia-dpr-multi-bf
@@ -40,7 +41,47 @@ class LuceneRetrieval():
             match = hits[qid]
             batch.append(self.scored_to_dict(match, num_guesses))
         return batch
+    
+    
+    def remove_adj_dup(self, text: str):
+        sents = text.split(". ")
+        sent = sents[0]
+        i = 1
+        if len(sent) < 5:
+            i = 2
+            sent = " ".join(sents[:2])
+        words = sent.split(" ")
+        diff = 2
 
+        seen = {}
+        
+        for word in words: 
+            word = word.lower()
+            if word in seen:
+                seen[word] += 1
+            else:
+                seen[word] = 1
+        ngram = []
+        final = []
+        for word in words: 
+            temp = word.lower()
+            if temp in seen and seen[temp] > 1 and temp not in ngram:
+                ngram.append(temp)
+                final.append(word)
+                seen[temp] -= 1
+            elif temp in ngram and seen[temp] >= 1:
+                pass
+            else:
+                final.append(word)
+                if diff <= 0:
+                    ngram = []
+                    diff = 2
+                else:
+                    diff -= 1
+                
+        sents = " ".join(final + sents[i:])
+        return sents
+    
     def scored_to_dict(self, obj, num_guesses):
         match = obj
         docs = []
@@ -48,6 +89,9 @@ class LuceneRetrieval():
             dict = json.loads(hit.lucene_document.get("raw"))
             contents = utils.lazy_split(dict["contents"], " ", 400)
             contents = (" ").join(contents)
+            contents = re.sub('\n', '', contents)
+            contents = self.remove_adj_dup(contents)
+
             score = hit.score
             id = dict["id"]
             docs.append({"id": id, "contents": contents, "confidence": score})
@@ -55,3 +99,6 @@ class LuceneRetrieval():
             lst = [({"id": -1, "contents": "", "confidence": 0}) for x in range(num_guesses - len(docs))]
             docs.extend(lst)
         return docs
+    
+
+
